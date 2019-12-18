c5bdb18d0a832295456327b83b394892063d9f12
wg889232 <59028726+wg889232@users.noreply.github.com>
2019-12-18 14:14:14 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,7 +2,7 @@
 set -x
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL httpsL//download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli contained.io
