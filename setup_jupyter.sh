7b30571b53d6d45a67d2d4e8f5f688b7ea471a71
wg889232 <59028726+wg889232@users.noreply.github.com>
2019-12-23 13:09:54 -0500

    Update setup_jupyter.sh

--- setup_jupyter.sh
+++ setup_jupyter.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
-sudo wget --no-check-certificate https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
-sudo bash Anaconda3-2019.10-Linux-x86_64.sh -b -p /opt/anaconda3
+wget --no-check-certificate https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/anaconda3
 echo "PATH=/opt/anaconda3/bin:$PATH" | sudo tee -a /etc/environment
+source /etc/environment
+conda install -c conda-forge notebook
